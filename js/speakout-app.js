d120fd56274a5296957f0a8aea18a85657f7e602
anami <anamicity@gmail.com>
2023-10-07 20:19:20 +0100

    basic haunted app working

--- js/speakout-app.js
+++ js/speakout-app.js
@@ -1,56 +1,14 @@
-import {LitElement, html, css} from "https://unpkg.com/lit-element?module";
+// import { html } from 'https://unpkg.com/lit?module';
+// import { component, useState } from 'https://unpkg.com/haunted/haunted.js';
+import { html, component, useState } from 'https://cdn.pika.dev/haunted';
 
-export class SpeakoutApp extends LitElement {
-  static properties = {
-    greeting: {},
-    planet: {},
-  };
-  // Styles are scoped to this element: they won't conflict with styles
-  // on the main page or in other components. Styling API can be exposed
-  // via CSS custom properties.
-  static styles = css`
-    :host {
-      display: inline-block;
-      padding: 10px;
-      background: lightgray;
-    }
-    .planet {
-      color: var(--planet-color, blue);
-    }
+function App() {
+  const [count, setCount] = useState(0);
+  return html`
+    <h2>Using lighterhtml!</h2>
+    <div>Count: ${count}</div>
+    <button part="button" @click=${() => setCount(count + 1)}>Increment</button>
   `;
-
-  constructor() {
-    super();
-    // Define reactive properties--updating a reactive property causes
-    // the component to update.
-    this.greeting = 'Hello';
-    this.planet = 'World';
-  }
-
-  // The render() method is called any time reactive properties change.
-  // Return HTML in a string template literal tagged with the `html`
-  // tag function to describe the component's internal DOM.
-  // Expressions can set attribute values, property values, event handlers,
-  // and child nodes/text.
-  render() {
-    return html`
-      <span @click=${this.togglePlanet}
-        >${this.greeting}
-        <span class="planet">${this.planet}</span>
-      </span>
-      <no-speech ?show="${false}"></no-speech>
-      <div>
-
-        <options-cog></options-cog>
-        <play-button></play-button>
-      </div>
-    `;
-  }
-
-  // Event handlers can update the state of @properties on the element
-  // instance, causing it to re-render
-  togglePlanet() {
-    this.planet = this.planet === 'World' ? 'Mars' : 'World';
-  }
 }
-customElements.define('speakout-app', SpeakoutApp);
+
+customElements.define('speakout-app', component(App));
